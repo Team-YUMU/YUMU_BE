9e755bbe974a2b85b4e032da5745f2c5a3200327
Rubi <quipu1@naver.com>
2024-03-06 17:05:06 +0900

    feat : 프로젝트 생성

---
+++ yumu_be/src/main/java/com/yumu/yumu_be/YumuBeApplication.java
@@ -0,0 +1,13 @@
+package com.yumu.yumu_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumuBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumuBeApplication.class, args);
+    }
+
+}
---
+++ yumu_be/src/test/java/com/yumu/yumu_be/YumuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumu.yumu_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumuBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
