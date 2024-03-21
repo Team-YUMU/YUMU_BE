d5a3a00c81ee7f2ca4b92ab26889dc1b1dcbc243
Rubi <quipu1@naver.com>
2024-03-21 17:39:37 +0900

    fix : cors의 allowed origin 변경

--- yumu_be/src/main/java/com/yumu/yumu_be/config/SecurityConfig.java
+++ yumu_be/src/main/java/com/yumu/yumu_be/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
