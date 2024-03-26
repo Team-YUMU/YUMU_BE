b8bf51d620e4e41a4e3133316d4be105fdf59de4
usnim <sms897@naver.com>
2024-03-26 22:31:40 +0900

    refactor : 웹소켓 시큐리티 설정 추가
    스프링시큐리티 웹소켓 경로 허용

--- yumu_be/src/main/java/com/yumu/yumu_be/config/SecurityConfig.java
+++ yumu_be/src/main/java/com/yumu/yumu_be/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/api/v1/auction/paging").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/api/v1/auction/{id}").permitAll()
-                                .requestMatchers("/socket-connect").permitAll()
+                                .requestMatchers("/ws-stomp/**").permitAll()
                                 .anyRequest().authenticated());
 
         http
