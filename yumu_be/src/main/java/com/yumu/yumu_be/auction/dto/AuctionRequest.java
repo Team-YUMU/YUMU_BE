29e3a33efb3e27ddb580169c67746761a593bc55
usnim <sms897@naver.com>
2024-03-28 01:00:08 +0900

    refactor : AuctionRequest 날짜 타입 직렬화 방법 변경

--- yumu_be/src/main/java/com/yumu/yumu_be/auction/dto/AuctionRequest.java
+++ yumu_be/src/main/java/com/yumu/yumu_be/auction/dto/AuctionRequest.java
@@ -2,6 +2,7 @@ package com.yumu.yumu_be.auction.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -13,18 +14,21 @@ public class AuctionRequest {
     private String artDescription;
     private String artSummary;
     private String artSize;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime artCreatedDate;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime auctionStartDate;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime auctionEndDate;
     private Integer defaultBid;
     private String notice;
     private String receiveType;
     private MultipartFile image;
 
-    public AuctionRequest(String artName, String artSubTitle, String artDescription, String artSummary, String artSize, LocalDateTime artCreatedDate, LocalDateTime auctionStartDate, LocalDateTime auctionEndDate, int defaultBid, String notice, String receiveType, MultipartFile image) {
+    public AuctionRequest(String artName, String artSubTitle, String artDescription, String artSummary, String artSize, LocalDateTime artCreatedDate, LocalDateTime auctionStartDate, LocalDateTime auctionEndDate, Integer defaultBid, String notice, String receiveType, MultipartFile image) {
         this.artName = artName;
         this.artSubTitle = artSubTitle;
         this.artDescription = artDescription;
